9a696c1e4c97a607be85fb8ae0e1d55ea7434a72
EdulimaProg <42524674+EdulimaProg@users.noreply.github.com>
2022-08-11 08:09:33 -0300

    add statics interface

--- src/main/java/com/example/lotto/LottoRepo/Interface/LottoStatitics.java
+++ src/main/java/com/example/lotto/LottoRepo/Interface/LottoStatitics.java
@@ -2,4 +2,8 @@ package com.example.lotto.LottoRepo.Interface;
 
 public abstract interface LottoStatitics {
     public abstract void getResultOfLastConquest();
+    public abstract void getPrimeNumbersOfLastConquest();
+    public abstract void getPairNumbersOfLastconquest();
+    public abstract void getMostAwardNumbersInLastConquest();
+    public abstract void confenceMyBetWithLastResults();
 }
